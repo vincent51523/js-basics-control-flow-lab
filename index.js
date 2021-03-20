5977954a602e9619f6a17689bcbf4da6028fb3a8
bin <vincent418@icloud.com>
2021-03-19 22:20:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+      return 'This one is on me!';
+    } else if (distance >= 2400) {
+      return 'I will gladly take your thirty bucks.';
+    } else if (distance >= 1500) 
+      return 'No can do.';
+  }
+  
 
 function ternaryCheckCity(){
-  // Write your code here!
+ let city 
 }
 
 function switchOnCharmFromTip(){
