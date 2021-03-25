34cbee1946ef71bd1d86f0d53213413bc624087e
bin <vincent418@icloud.com>
2021-03-24 22:18:57 -0500

    commit tree 8f6c67fb34e1e7cdc5aa505d1e7e94e4572a830d

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 function scuberGreetingForFeet(distance){
   if (distance <= 400) {
       return 'This one is on me!';
-    } else if (distance >= 2400) {
+    } else if (2000 < distance && distance < 2500) {
       return 'I will gladly take your thirty bucks.';
     } else if (distance >= 1500) 
       return 'No can do.';
   }
   
 
-function ternaryCheckCity(){
- let city 
+function ternaryCheckCity(city){
+ return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }   
+  
 }
